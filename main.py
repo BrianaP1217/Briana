fc4c3bc025fdd4cf9296ee84b4d7291b7a21378f
BrianaP1217 <152335283+BrianaP1217@users.noreply.github.com>
2023-11-29 14:13:40 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+rose_thrown = "<~~^~((0)"
+
+i=0
+while i<5:
+  print(rose_thrown)
+  i+=1
+
+print("The roses were thrown",i,"times.")
+
+cat="(^00^)---~~"
+
+i=0
+while i<5:
+  print(cat)
+  i+=1
+
+print("There are",i,"cats running after you.")
